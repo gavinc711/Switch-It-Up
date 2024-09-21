f336854713516af9dc3bd2f9f21a120f56989071
Gavin Cholette <149112460+gavinc711@users.noreply.github.com>
2024-09-21 14:56:12 -0400

    Win anim
    Little guy looks in the telescope

--- Assets/Scripts/Win.cs
+++ Assets/Scripts/Win.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class Win : MonoBehaviour
 {
     public GameObject youWin;
+	public SpriteRenderer sprites;
+	public Sprite newSprite;
     
+	void Awake()
+	{
+		sprites = GetComponent<SpriteRenderer>();
+	}
+	
     void OnTriggerEnter2D(Collider2D other)
     {
-        PlayerScript player = other.GetComponent<PlayerScript>();
-        
-        if(player != null)
-        {
-            youWin.SetActive(true);
-        }
-        else
-        {
-            youWin.SetActive(false);
-        }
+        if(other.gameObject.tag == "Player")
+		{
+			other.gameObject.SetActive(false);
+			sprites.sprite = newSprite;
+			StartCoroutine(WaitForMenu(2f));
+		}
     }
-
+	
+	private IEnumerator WaitForMenu(float delay)
+	{
+		yield return new WaitForSeconds(delay);
+		youWin.SetActive(true);
+	}
 }
