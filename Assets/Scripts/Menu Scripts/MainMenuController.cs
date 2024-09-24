27cd2c180408052af8cc1bfcb0e06648cdadfe0d
Dante <dantec830@gmail.com>
2024-09-24 13:55:23 -0400

    Good god

--- Assets/Scripts/Menu Scripts/MainMenuController.cs
+++ Assets/Scripts/Menu Scripts/MainMenuController.cs
@@ -14,6 +14,32 @@ public class MainMenuController : MonoBehaviour
     public GameObject credits;
     public GameObject gallery;
     public GameObject lvls;
+    public Score ScoreScript;
+
+    public GameObject partyImage;
+    public GameObject alienImage;
+    public GameObject golfImage;
+    // public GameObject csImage;
+    public GameObject conceptImage;
+
+
+    public GameObject unlock;
+    //backgrounds
+    public GameObject purpleImage;
+    public GameObject redImage;
+    public GameObject grayImage;
+    public GameObject greenImage;
+    public GameObject blueImage;
+
+    public GameObject partyLock;
+    public GameObject conceptLock;
+    public GameObject alienLock;
+    public GameObject golfLock;
+    public GameObject purpleLock;
+    public GameObject redLock;
+    public GameObject grayLock;
+    public GameObject greenLock;
+    public GameObject blueLock;
 
     public int currentSceneIndex;
     public int nextSceneIndex;
@@ -52,6 +78,68 @@ public class MainMenuController : MonoBehaviour
     public void Gallery()
     {
         gallery.gameObject.SetActive(true);
+        GameObject targetObject = GameObject.Find("ScoreTracker");
+        if (targetObject != null)
+        {
+            ScoreScript = targetObject.GetComponent<Score>();
+        }
+        if (ScoreScript != null)
+        {
+            Debug.Log("Player found score script.");
+        }
+        else
+        {
+            Debug.LogError("script reference is not found on the target gameobject!");
+        }
+
+        if(ScoreScript.score >= 2)
+        {
+            unlock.SetActive(true);
+            partyLock.SetActive(false);
+            partyImage.SetActive(true);
+        }
+        if (ScoreScript.score >= 4)
+        {
+            conceptLock.SetActive(false);
+            conceptImage.SetActive(true);
+        }
+        if (ScoreScript.score >= 6)
+        {
+            alienLock.SetActive(false);
+            alienImage.SetActive(true);
+        }
+        if (ScoreScript.score >= 8)
+        {
+            golfLock.SetActive(false);
+            golfImage.SetActive(true);
+        }
+        if (ScoreScript.score >= 10)
+        {
+            purpleLock.SetActive(false);
+            purpleImage.SetActive(true);
+        }
+        if (ScoreScript.score >= 12)
+        {
+            redLock.SetActive(false);
+            redImage.SetActive(true);
+        }
+        if (ScoreScript.score >= 14)
+        {
+            grayLock.SetActive(false);
+            grayImage.SetActive(true);
+        }
+        if (ScoreScript.score >= 16)
+        {
+            greenLock.SetActive(false);
+            greenImage.SetActive(true);
+        }
+        if(ScoreScript.score >= 18)
+        {
+            blueLock.SetActive(false);
+            blueImage.SetActive(true);
+        }
+
+
     }
 
     public void Credits()
